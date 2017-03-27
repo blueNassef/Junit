ea41fb27c6b7b37c16f6ba20c177828ea33db506
root <trois4ever@gmail.com>
2017-03-27 02:49:07 +0200

    splash_done

--- app/src/main/java/com/bluen/junit/MainActivity.java
+++ app/src/main/java/com/bluen/junit/MainActivity.java
@@ -1,13 +1,24 @@
 package com.bluen.junit;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private AlphaAnimation anim;
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        anim= new AlphaAnimation(5f, 0f);
+        anim.setDuration(5000);
+        logo= (ImageView) findViewById(R.id.logo);
+        logo.startAnimation(anim);
+
     }
 }
